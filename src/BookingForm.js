a367ee4e28dbcaa42552b155cfa4c8bf6321a200
Etiene Novais <novais.etiene@gmail.com>
2023-01-13 18:32:37 +0100

    UX improvements

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -22,10 +22,12 @@ export default function BookingForm(props){
         <form onSubmit={handleSubmit} style={{display: 'grid', maxWidth: '200px', gap: '20px'}}>
             <label htmlFor="res-date">Choose date</label>
             <input type="date" id="res-date" value={date} onChange={handleDateChange}/>
+            {date==='' && <label htmlFor="res-date" className='message'>Please select a date</label>}
             <label htmlFor="res-time">Choose time</label>
             <select id="res-time " value={time} onChange={e=>setTime(e.target.value)}>
                 {props.availableTimes.map(x => <option>{x}</option>)}
             </select>
+            {props.availableTimes.length===0 && <label htmlFor="res-date" className='message'>No available times for the choosen date</label>}
             <label htmlFor="guests">Number of guests</label>
             <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={e=>setGuests(e.target.value)}/>
             <label htmlFor="occasion">Occasion</label>
@@ -33,7 +35,7 @@ export default function BookingForm(props){
                 <option>Birthday</option>
                 <option>Anniversary</option>
             </select>
-            <input type="submit" value="Make Your reservation" />
+            <input aria-label='On Click' disabled={props.availableTimes.length===0 || date===''} type="submit" value="Make Your reservation" />
         </form>
     );
 }
